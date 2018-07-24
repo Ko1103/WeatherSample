6496e80284719ed744788d3bd5afb4a64ed623b6
KO-YAMAKO <ko.foot1103@gmail.com>
2018-07-24 21:58:08 +0900

    add texts

---
+++ WeatherSample/Texts.swift
@@ -0,0 +1,17 @@
+//
+//  Texts.swift
+//  WeatherSample
+//
+//  Created by 山浦功 on 2018/07/24.
+//  Copyright © 2018年 KoYamaura. All rights reserved.
+//
+
+import Foundation
+
+struct Text {
+    struct Dialogues {
+        static let errorTitle = "Whoops!"
+        static let errorMessage = "Something went wrong"
+        static let okayText =  "Okay"
+    }
+}
