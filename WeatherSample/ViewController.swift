896879f74a56673452edcacf014d44ee4ca51398
KO-YAMAKO <ko.foot1103@gmail.com>
2018-07-24 17:05:12 +0900

    create new project

---
+++ WeatherSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherSample
+//
+//  Created by 山浦功 on 2018/07/24.
+//  Copyright © 2018年 KoYamaura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
