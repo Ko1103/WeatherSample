710e390edab4cfe3937179780d14423dc8b42b96
KO-YAMAKO <ko.foot1103@gmail.com>
2018-07-25 15:02:49 +0900

    add forecastViewCell

---
+++ WeatherSample/ForecastTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  ForecastTableViewCell.swift
+//  WeatherSample
+//
+//  Created by 山浦功 on 2018/07/25.
+//  Copyright © 2018年 KoYamaura. All rights reserved.
+//
+
+import UIKit
+
+class ForecastTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var cityDegreesLabel: UILabel!
+    @IBOutlet weak var weatherMessageLabel: UILabel!
+    @IBOutlet weak var weatherImageOutlet: UIImageView!
+    
+}
