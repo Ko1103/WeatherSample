a86c833aa546f2dc8eaf87c14dae21798aa265d0
KO-YAMAKO <ko.foot1103@gmail.com>
2018-07-30 15:56:40 +0900

    set tableviewDelegates

--- WeatherSample/WeatherOverviewViewController.swift
+++ WeatherSample/WeatherOverviewViewController.swift
@@ -17,33 +17,63 @@ struct ForecastModel {
     let temp: String
 }
 
-class WeatherOverviewViewController: UIViewController {
+
+final class WeatherOverviewViewController: UIViewController {
 
     private var viewModel: WeatherViewModel!
     private let disposeBag = DisposeBag()
     
-    
+    @IBOutlet weak var forecastsTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        forecastsTableView.delegate = self
+        self.viewModel = WeatherViewModel(weatherService: WeatherAPIService())
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    private var tableViewData: [(day: String, forecasts: [ForecastModel])]? {
+        didSet { //値が変更された時にしたい処理を書く
+            // oldValueで変更前の値を取得することができる。
+            self.forecastsTableView.reloadData()
+        }
     }
-    */
-
 }
+
+extension WeatherOverviewViewController: UITableViewDataSource , RxTableViewDataSourceType {
+    
+    func tableView(_ tableView: UITableView, observedEvent: Event<[(day: String, forecasts: [ForecastModel])]>) {
+        switch observedEvent {
+        case .next(let items):
+            tableViewData = items
+        case .error(let error):
+            print(error)
+        case .completed:
+            tableViewData = nil
+        }
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return self.tableViewData?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.tableViewData?[section].forecasts.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return self.tableViewData![section].day
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "forecastCell") as! ForecastTableViewCell
+        guard let forecast = tableViewData?[indexPath.section].forecasts[indexPath.row] else {
+            return cell
+        }
+        cell.cityDegreesLabel.text = forecast.temp
+        cell.dateLabel.text = forecast.time
+        cell.weatherMessageLabel.text = forecast.description
+        return cell
+    }
+}
+
+extension WeatherOverviewViewController: UITableViewDelegate {}
